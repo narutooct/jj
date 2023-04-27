f1c442cd4881319772e3faea5ad9b87f27b5ba1d
narutooct <131933763+narutooct@users.noreply.github.com>
2023-04-27 09:31:09 +0530

    Update Nqueens.py

--- Nqueens.py
+++ Nqueens.py
@@ -1,32 +1,32 @@
 def is_safe(board, row, col, n):
-    #ROW OF LEFT
+
     for i in range(col):
         if board[row][i] == 1:
             return False
     
-    #UPPER DIAGONAL LEFT
+
     for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
     
-    #LOWER DIAGONAL LEFT
+
     for i, j in zip(range(row, n), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
 
     return True
 def solve_n_queens(board, col, n):
-    #ALL QUEENS PLACED
+  
     if col == n:
         return True
-    #TRY THE QUEEN IN EACH ROW
+  
     for i in range(n):
         if is_safe(board, i, col, n):
             board[i][col] = 1
-            #RECURSIVE TO PLACE THE QUEENS
+           
             if solve_n_queens(board, col+1, n):
                 return True
-            #BACTRACING IF QUEENS CLASHES
+          
             board[i][col] = 0
     return False
 
